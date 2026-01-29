5cb6804466d21b7e476a959646002f68feb4c318
MikeSays-1 <akaaone@gmail.com>
2026-01-29 18:55:05 -0500

    Add files via upload

---
+++ ad-script/AD PS Script.ps1
@@ -0,0 +1,24 @@
+﻿# ----- Edit these Variables for your own Use Case ----- #
+$PASSWORD_FOR_USERS   = "Password1"
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$USER_FIRST_LAST_LIST = Get-Content "$scriptDir\names.txt"
+# ------------------------------------------------------ #
+
+$password = ConvertTo-SecureString $PASSWORD_FOR_USERS -AsPlainText -Force
+
+foreach ($n in $USER_FIRST_LAST_LIST) {
+    $first = $n.Split(" ")[0].ToLower()
+    $last = $n.Split(" ")[1].ToLower()
+    $username = $first.Substring(0,1) + '.' + $last
+    Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Cyan
+    
+    New-AdUser -AccountPassword $password `
+               -GivenName $first `
+               -Surname $last `
+               -DisplayName $username `
+               -Name $username `
+               -EmployeeID $username `
+               -PasswordNeverExpires $true `
+               -Path "ou=_EMPLOYEES,$(([ADSI]`"").distinguishedName)" `
+               -Enabled $true
+}
